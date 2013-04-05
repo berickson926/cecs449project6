07c92ef7b4996ec872b4d7ee1e66931ddb7770bf
Bret Erickson <berickson926@gmail.com>
2013-04-04 20:31:54 -0700

    Fixed dual window support. Person drawn in separate window from stroke font

--- Project6/Project6/Driver.cpp
+++ Project6/Project6/Driver.cpp
@@ -12,16 +12,21 @@
 
 using namespace std;
 
-int screenWidth = 500;
-int screenHeight = 500;
+int screenWidth1 = 500;
+int screenHeight1 = 500;
 
-int L = 450;
+int screenWidth2 = 500;
+int screenHeight2 = 500;
+
+int L1, L2;
 
 int rotateAngle, yDisplacement;
 float zoom;
 
 Person *person;
 FontWriter *fontWriter;
+
+int personWindow, strokeFontWindow;
 
 void drawPerson()
 {
@@ -42,86 +47,96 @@ void drawText()
 	glPopMatrix();
 }//End drawText
 
-void reshape(int w, int h)
+void reshapePerson(int w, int h)
 {
 	//update screen dim
-	screenWidth = w;
-	screenHeight =h;
+	screenWidth1 = w;
+	screenHeight1 =h;
 
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	GLfloat aspect = (GLfloat) screenWidth / (GLfloat)screenHeight;
+	GLfloat aspect = (GLfloat) screenWidth1 / (GLfloat)screenHeight1;
 
 	////setup worldwindow
 	glMatrixMode(GL_PROJECTION);				
 	glLoadIdentity();							
 
-	glViewport(0,0,screenWidth, screenHeight);
+	glViewport(0,0,screenWidth1, screenHeight1);
 
-	if(screenWidth <= screenHeight)
-		gluOrtho2D(-L, L, -L/aspect, L/aspect);
+	if(screenWidth1 <= screenHeight1)
+		gluOrtho2D(-L1, L1, -L1/aspect, L1/aspect);
 	else
-		gluOrtho2D(-L*aspect, L*aspect, -L, L);
+		gluOrtho2D(-L1*aspect, L1*aspect, -L1, L1);
 	
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 	glutPostRedisplay();
-}//end reshape
+}//end reshapePerson
 
-void display()
+void reshapeStrokeFont(int w, int h)
+{
+	//update screen dim
+	screenWidth2 = w;
+	screenHeight2 =h;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	GLfloat aspect = (GLfloat) screenWidth2 / (GLfloat)screenHeight2;
+
+	////setup worldwindow
+	glMatrixMode(GL_PROJECTION);				
+	glLoadIdentity();							
+
+	glViewport(0,0,screenWidth2, screenHeight2);
+
+	if(screenWidth2 <= screenHeight2)
+		gluOrtho2D(-L2, L2, -L2/aspect, L2/aspect);
+	else
+		gluOrtho2D(-L2*aspect, L2*aspect, -L2, L2);
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glutPostRedisplay();
+}//end reshapeStrokeFont
+
+void displayPerson()
 {
 	
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClearColor(0, 0, 0, 0);	//Paint black background
 
 	//Person Screen on the left
-	glViewport(0,0, screenWidth, screenHeight);
-	//drawPerson();
+	glViewport(0,0, screenWidth1, screenHeight1);
+	drawPerson();
 
-	//Text Screen on the right
-	//glViewport(screenWidth/2, 0, screenWidth/2, screenHeight);
-	drawText();
+	////Draw x-y axis for debugging purposes
+	//glColor3f(1,1,1);
+	//glBegin(GL_LINES);
+	//	glVertex2i(0, -L1);
+	//	glVertex2i(0, L1);
+	//glEnd();
 
-	//Draw x-y axis for debugging purposes
-	glColor3f(1,1,1);
-	glBegin(GL_LINES);
-		glVertex2i(0, -L);
-		glVertex2i(0, L);
-	glEnd();
-
-	glBegin(GL_LINES);
-		glVertex2i(-L, 0);
-		glVertex2i(L, 0);
-	glEnd();
+	//glBegin(GL_LINES);
+	//	glVertex2i(-L1, 0);
+	//	glVertex2i(L1, 0);
+	//glEnd();
 	
 	glutSwapBuffers();
-}//end display
+}//end displayPerson
 
-void initialize()
+void displayText()
 {
-	cout << "Keyboard commands: "<< endl;
-	cout << "\t W = Translate up on Y-axis" << endl;
-	cout << "\t S = Translate down on Y-Axis" << endl;
-	cout << "\t Q = Rotate text counter clockwise" << endl;
-	cout << "\t E = Rotate text clockwise" << endl;
-	cout << "\t A = Zoom out " << endl;
-	cout << "\t D = Zoom in " << endl;
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0, 0, 0, 0);	//Paint black background
 
-	glutInitWindowSize(screenWidth, screenHeight);
-	glutInitWindowPosition(50, 50);
-	glutCreateWindow("People & Fonts");
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(-L,L,-L,L);
+	//Person Screen on the left
+	glViewport(0,0, screenWidth2, screenHeight2);
+	drawText();
 	
-	person = new Person();
-	fontWriter = new FontWriter();
-
-	zoom = 1;
-	rotateAngle = yDisplacement = 0;
-}//end initialize
+	glutSwapBuffers();
+}//end displayText
 
 void keyboard(unsigned char key, int x, int y)
 {
@@ -171,16 +186,62 @@ void keyboard(unsigned char key, int x, int y)
 	}
 }//end keyboard
 
+//Pre: glutInit & glutInitDisplayMode must be called prior to execution
+//Post:
+void initialize()
+{
+	cout << "Keyboard commands: "<< endl;
+	cout << "\t W = Translate up on Y-axis" << endl;
+	cout << "\t S = Translate down on Y-Axis" << endl;
+	cout << "\t Q = Rotate text counter clockwise" << endl;
+	cout << "\t E = Rotate text clockwise" << endl;
+	cout << "\t A = Zoom out " << endl;
+	cout << "\t D = Zoom in " << endl;
+
+	L1 = L2 = 450;
+
+	glutInitWindowSize(screenWidth1, screenHeight1);
+	//Create Two Windows, one for the Person the other for stroke font
+
+	//First Window:
+	personWindow = glutCreateWindow("People");
+	glutInitWindowPosition(50, 50);
+	//Register callbacks
+	glutDisplayFunc(displayPerson);
+	glutReshapeFunc(reshapePerson);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-L1,L1,-L1,L1);
+
+	person = new Person();
+
+	//Second Window:
+	glutInitWindowPosition(600,0);
+	strokeFontWindow = glutCreateWindow("Stroke Font");
+	
+	//Register Callbacks
+	glutDisplayFunc(displayText);
+	glutReshapeFunc(reshapeStrokeFont);
+	glutKeyboardFunc(keyboard);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-L2,L2,-L2,L2);
+
+	fontWriter = new FontWriter();
+
+	zoom = 1;
+	rotateAngle = yDisplacement = 0;
+}//end initialize
+
+
 void main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 
 	initialize();//Initial setup
-
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape);
-	glutKeyboardFunc(keyboard);
 
 	glutMainLoop();
 
