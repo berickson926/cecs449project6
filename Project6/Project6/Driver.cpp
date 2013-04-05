e34b4b9f5316a78170b6980b62f9b977395a2280
Bret Erickson <berickson926@gmail.com>
2013-04-04 21:09:17 -0700

    Additional context pane with alternate OpenGL font demo

--- Project6/Project6/Driver.cpp
+++ Project6/Project6/Driver.cpp
@@ -12,26 +12,28 @@
 
 using namespace std;
 
-int screenWidth1 = 500;
-int screenHeight1 = 500;
+int screenWidth1 = 400;
+int screenHeight1 = 400;
 
-int screenWidth2 = 500;
-int screenHeight2 = 500;
+int screenWidth2 = 400;
+int screenHeight2 = 400;
 
-int L1, L2;
+int screenWidth3 = 400;
+int screenHeight3 = 400;
 
-int rotateAngle, yDisplacement;
-float zoom;
+int L1, L2, L3;                                       
 
-Person *person;
-FontWriter *fontWriter;
+int rotateAngle, yDisplacement, rotateAngle2, yDisplacement2;
+float zoom, zoom2;                                           
 
-int personWindow, strokeFontWindow;
+Person *person;                                       //Object implementation of single person rendering
+FontWriter *fontWriter;                               //Object that implements stroke font functionality
+
+int personWindow, strokeFontWindow, openGLFontWindow; //Window ID for context switching
 
 void drawPerson()
 {
 	glColor3f(1,0,0);
-
 	person->drawPerson();
 	glFlush();
 
@@ -101,27 +103,80 @@ void reshapeStrokeFont(int w, int h)
 	glutPostRedisplay();
 }//end reshapeStrokeFont
 
+void reshapeOpenGLFont(int w, int h)
+{
+	//update screen dim
+	screenWidth3 = w;
+	screenHeight3 =h;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	GLfloat aspect = (GLfloat) screenWidth3 / (GLfloat)screenHeight3;
+
+	////setup worldwindow
+	glMatrixMode(GL_PROJECTION);				
+	glLoadIdentity();							
+
+	glViewport(0,0,screenWidth2, screenHeight2);
+
+	if(screenWidth2 <= screenHeight2)
+		gluOrtho2D(-L3, L3, -L3/aspect, L3/aspect);
+	else
+		gluOrtho2D(-L3*aspect, L3*aspect, -L3, L3);
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glutPostRedisplay();
+}//end reshapeOpenGLFont
+
+void draw_string_stroke(void *font, const char* string)
+{ 
+	glPushMatrix();
+		glTranslatef(-125,-50,0);
+		while(*string)
+			glutStrokeCharacter(font, *string++);
+	glPopMatrix();
+}//end draw_string_stroke
+
+void displayOpenGLFont()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glClearColor(0, 0, 0, 0);	//Paint black background
+
+	glViewport(0,0, screenWidth3, screenHeight3);
+
+	glColor3f(1,1,1);
+	glPushMatrix();
+		glTranslatef(1, yDisplacement2, 1);
+		glScalef(zoom2,zoom2,1);
+		glRotatef(rotateAngle2,0,0,1);
+		draw_string_stroke(GLUT_STROKE_ROMAN, "DDD");
+	glPopMatrix();
+
+	//Draw x-y axis for debugging purposes
+	glColor3f(1,1,1);
+	glBegin(GL_LINES);
+		glVertex2i(0, -L3);
+		glVertex2i(0, L3);
+	glEnd();
+
+	glBegin(GL_LINES);
+		glVertex2i(-L3, 0);
+		glVertex2i(L3, 0);
+	glEnd();
+
+	glutSwapBuffers();
+}//end displayOpenGLFont()
+
 void displayPerson()
 {
-	
 	glClear(GL_COLOR_BUFFER_BIT);
 	glClearColor(0, 0, 0, 0);	//Paint black background
 
 	//Person Screen on the left
 	glViewport(0,0, screenWidth1, screenHeight1);
 	drawPerson();
-
-	////Draw x-y axis for debugging purposes
-	//glColor3f(1,1,1);
-	//glBegin(GL_LINES);
-	//	glVertex2i(0, -L1);
-	//	glVertex2i(0, L1);
-	//glEnd();
-
-	//glBegin(GL_LINES);
-	//	glVertex2i(-L1, 0);
-	//	glVertex2i(L1, 0);
-	//glEnd();
 	
 	glutSwapBuffers();
 }//end displayPerson
@@ -186,6 +241,54 @@ void keyboard(unsigned char key, int x, int y)
 	}
 }//end keyboard
 
+void keyboardOpenGLFont(unsigned char key, int x, int y)
+{
+	switch (key) 
+	{
+	case 'q':
+		//Rotate left
+		rotateAngle2++;
+		glutPostRedisplay();
+		break;
+
+	case 'e':
+		//Rotate right
+		rotateAngle2--;
+		glutPostRedisplay();
+		break;
+
+	case 'w':
+		//Move up
+		yDisplacement2 += 2;
+		glutPostRedisplay();
+		break;
+
+	case 's':
+		//Move down
+		yDisplacement2 -= 2;
+		glutPostRedisplay();
+		break;
+
+	case 'a':
+		//Zoom out
+		zoom2 -= .1;
+		glutPostRedisplay();
+		break;
+
+	case 'd':
+		//Zoom in
+		zoom2 += .1;
+		glutPostRedisplay();
+		break;
+
+	case ' ':
+		glutPostRedisplay();
+		break;
+	case 27:
+		exit(0);
+	}
+}//end keyboardOpenGLFont
+
 //Pre: glutInit & glutInitDisplayMode must be called prior to execution
 //Post:
 void initialize()
@@ -198,7 +301,7 @@ void initialize()
 	cout << "\t A = Zoom out " << endl;
 	cout << "\t D = Zoom in " << endl;
 
-	L1 = L2 = 450;
+	L1 = L2 = L3 = 450;
 
 	glutInitWindowSize(screenWidth1, screenHeight1);
 	//Create Two Windows, one for the Person the other for stroke font
@@ -217,7 +320,7 @@ void initialize()
 	person = new Person();
 
 	//Second Window:
-	glutInitWindowPosition(600,0);
+	glutInitWindowPosition(425,0);
 	strokeFontWindow = glutCreateWindow("Stroke Font");
 	
 	//Register Callbacks
@@ -233,6 +336,22 @@ void initialize()
 
 	zoom = 1;
 	rotateAngle = yDisplacement = 0;
+
+	//Third window:
+	glutInitWindowPosition(0,460);
+	openGLFontWindow = glutCreateWindow("OpenGL Font");
+
+	//Register Callbacks
+	glutDisplayFunc(displayOpenGLFont);
+	glutReshapeFunc(reshapeOpenGLFont);
+	glutKeyboardFunc(keyboardOpenGLFont);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(-L3, L3, -L3, L3);
+
+	zoom2 = 1;
+	rotateAngle2 = yDisplacement2 = 0;
 }//end initialize
 
 
